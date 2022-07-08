b602c4a640df602a582df276b04fd0d50bb69472
Charlie Welsh <charlie@allotrope.dev>
2022-07-08 15:55:27 -0700

    Add health code beginnings

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -4,28 +4,36 @@ import Vapor
 struct TodoController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let todos = routes.grouped("todos")
-        todos.get(use: index)
+//        todos.get(use: index)
         todos.post(use: create)
-        todos.group(":todoID") { todo in
-            todo.delete(use: delete)
-        }
+//        todos.group(":todoID") { todo in
+//            todo.delete(use: delete)
+//        }
     }
 
-    func index(req: Request) async throws -> [Todo] {
-        try await Todo.query(on: req.db).all()
+//    func index(req: Request) async throws -> [Todo] {
+//        try await Todo.query(on: req.db).all()
+//    }
+//
+
+	struct HealthCardTest: Content {
+		var number: UInt8
+	}
+
+
+    func create(req: Request) async throws -> String {
+
+
+//        let todo = try req.content.decode(Todo.self)
+//        try await todo.save(on: req.db)
+//        return todo
     }
 
-    func create(req: Request) async throws -> Todo {
-        let todo = try req.content.decode(Todo.self)
-        try await todo.save(on: req.db)
-        return todo
-    }
-
-    func delete(req: Request) async throws -> HTTPStatus {
-        guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
-            throw Abort(.notFound)
-        }
-        try await todo.delete(on: req.db)
-        return .noContent
-    }
+//    func delete(req: Request) async throws -> HTTPStatus {
+//        guard let todo = try await Todo.find(req.parameters.get("todoID"), on: req.db) else {
+//            throw Abort(.notFound)
+//        }
+//        try await todo.delete(on: req.db)
+//        return .noContent
+//    }
 }
